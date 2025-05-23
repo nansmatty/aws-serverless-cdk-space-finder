d0afc3136875afc6bac4864ae925ff5b48485715
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-05-23 17:45:34 +0530

    adding aws sdk and listing the s3 buckets

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 import { ITable } from 'aws-cdk-lib/aws-dynamodb';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
@@ -24,6 +25,17 @@ export class LambdaStack extends Stack {
 				SPACES_TABLE_NAME: props.spacesTableName.tableName,
 			},
 		});
+
+		// Grant the Lambda function permissions to access the s3 buckets
+		// adding resources policy * is not recommended in production but for this example we are going to use it
+
+		helloLambda.addToRolePolicy(
+			new PolicyStatement({
+				effect: Effect.ALLOW,
+				actions: ['s3:ListAllMyBuckets', 's3:ListBucket'],
+				resources: ['*'],
+			})
+		);
 
 		this.helloLambdaIntegration = new LambdaIntegration(helloLambda);
 	}
--- src/services/hello.ts
+++ src/services/hello.ts
@@ -1,23 +1,35 @@
+import { ListBucketsCommand, S3Client } from '@aws-sdk/client-s3';
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 import { v4 } from 'uuid';
+
+const s3Client = new S3Client({});
 
 // APIGatewayProxyEvent is the type for the event parameter because if this handler is accessed via API Gateway
 // Context is the type for the context parameter because it contains information about the invocation, function, and execution environment
 
 async function handler(event: APIGatewayProxyEvent, context: Context) {
-	const response: APIGatewayProxyResult = {
-		statusCode: 200,
-		body: JSON.stringify({
-			message: 'Hello from the serverless function!',
-			randomId: v4(),
-			tableName: `Hello the DynamoDb Table Name is: ${process.env.SPACES_TABLE_NAME}`,
-		}),
-	};
+	// List all S3 buckets
+	try {
+		const data = await s3Client.send(new ListBucketsCommand({}));
+		console.log('Success', data.Buckets);
 
-	// Log the event and context to the'
-	console.log('Event:', JSON.stringify(event));
+		const response: APIGatewayProxyResult = {
+			statusCode: 200,
+			body: JSON.stringify({
+				message: 'Hello from the serverless function!',
+				randomId: v4(),
+				tableName: `Hello the DynamoDb Table Name is: ${process.env.SPACES_TABLE_NAME}`,
+				s3BucketsList: [...data.Buckets],
+			}),
+		};
 
-	return response;
+		// Log the event and context to the'
+		console.log('Event:', JSON.stringify(event));
+
+		return response;
+	} catch (err) {
+		console.log('Error', err);
+	}
 }
 
 export { handler };
