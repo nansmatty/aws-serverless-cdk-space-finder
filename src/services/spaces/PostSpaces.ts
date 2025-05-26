b6e5fa9abfc29585b08781a964fd435f5d9458e1
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-05-26 18:42:37 +0530

    Data validation config added

--- src/services/spaces/PostSpaces.ts
+++ src/services/spaces/PostSpaces.ts
@@ -2,12 +2,14 @@ import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
 import { marshall } from '@aws-sdk/util-dynamodb';
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { v4 } from 'uuid';
+import { validateSpaceEntry } from '../shared/DataValidator';
 
 export async function postSpaces(event: APIGatewayProxyEvent, ddbClient: DynamoDBClient): Promise<APIGatewayProxyResult> {
 	const randomId = v4();
 	const item = JSON.parse(event.body || '{}');
 
 	item.id = randomId;
+	validateSpaceEntry(item);
 
 	//without marshalling, the item will look like this:
 	// Item: {
@@ -15,13 +17,16 @@ export async function postSpaces(event: APIGatewayProxyEvent, ddbClient: DynamoD
 	// 	location: { S: item.location },
 	// },
 
+	// with marshalling, the item will look like this:
+	// marshall({
+	// 	id: randomId,
+	// 	location: item.location,
+	// }),
+
 	const result = await ddbClient.send(
 		new PutItemCommand({
 			TableName: process.env.SPACES_TABLE_NAME || '',
-			Item: marshall({
-				id: randomId,
-				location: item.location,
-			}),
+			Item: marshall(item),
 		})
 	);
 
--- test/launcher.ts
+++ test/launcher.ts
@@ -5,13 +5,13 @@ process.env.SPACES_TABLE_NAME = 'SpaceStack-02ca41ab5897';
 
 handler(
 	{
-		httpMethod: 'DELETE',
-		queryStringParameters: {
-			id: '55d0c9ae-5c3b-48b1-bdf4-f94559c2cbcb', // Example space ID
-		},
-		// body: JSON.stringify({
-		// 	location: 'Paris',
-		// }),
+		httpMethod: 'POST',
+		// queryStringParameters: {
+		// 	id: '55d0c9ae-5c3b-48b1-bdf4-f94559c2cbcb', // Example space ID
+		// },
+		body: JSON.stringify({
+			location: 'Paris',
+		}),
 	} as any,
 	{} as any
 )
