c9be3c7c3f5d9de27b3161436782ba00633f88e0
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-05-26 23:30:18 +0530

    cognito stack setup using cdk

--- src/infra/Launcher.ts
+++ src/infra/Launcher.ts
@@ -2,12 +2,15 @@ import { App } from 'aws-cdk-lib';
 import { DataStack } from './stacks/Datastack';
 import { LambdaStack } from './stacks/LambdaStack';
 import { ApiStack } from './stacks/ApiStack';
+import { AuthStack } from './stacks/AuthStack';
 
 const app = new App();
 const dataStack = new DataStack(app, 'DataStack');
 const lambdaStack = new LambdaStack(app, 'LambdaStack', {
 	spacesTableName: dataStack.spacesTable,
 });
+
+new AuthStack(app, 'AuthStack');
 new ApiStack(app, 'ApiStack', {
 	spacesLambdaIntegration: lambdaStack.spacesLambdaIntegration,
 });
