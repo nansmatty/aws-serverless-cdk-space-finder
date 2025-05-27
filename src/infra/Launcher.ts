9dfe9307dd71c6d70692dbea8b17cf40eb34210d
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-05-27 18:02:44 +0530

    Connecting the auth stack with api gateway

--- src/infra/Launcher.ts
+++ src/infra/Launcher.ts
@@ -10,7 +10,11 @@ const lambdaStack = new LambdaStack(app, 'LambdaStack', {
 	spacesTableName: dataStack.spacesTable,
 });
 
-new AuthStack(app, 'AuthStack');
+const authStack = new AuthStack(app, 'AuthStack');
+
+// Create the API stack, passing the Lambda integration and user pool from the other stacks for security and functionality
+
 new ApiStack(app, 'ApiStack', {
 	spacesLambdaIntegration: lambdaStack.spacesLambdaIntegration,
+	userPool: authStack.userPool,
 });
--- src/infra/stacks/ApiStack.ts
+++ src/infra/stacks/ApiStack.ts
@@ -1,9 +1,11 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { AuthorizationType, CognitoUserPoolsAuthorizer, LambdaIntegration, MethodOptions, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { IUserPool } from 'aws-cdk-lib/aws-cognito';
 import { Construct } from 'constructs';
 
 interface ApiStackProps extends StackProps {
 	spacesLambdaIntegration: LambdaIntegration;
+	userPool: IUserPool;
 }
 
 export class ApiStack extends Stack {
@@ -11,10 +13,29 @@ export class ApiStack extends Stack {
 		super(scope, id, props);
 
 		const api = new RestApi(this, 'SpaceApi');
+
+		// Create a Cognito User Pools authorizer for the API
+		// This authorizer will use the user pool created in the AuthStack
+
+		const authorizer = new CognitoUserPoolsAuthorizer(this, 'SpaceApiAuthorizer', {
+			cognitoUserPools: [props.userPool],
+			identitySource: 'method.request.header.Authorization',
+		});
+
+		authorizer._attachToApi(api);
+		const optionsWithAuth: MethodOptions = {
+			authorizationType: AuthorizationType.COGNITO,
+			authorizer: {
+				authorizerId: authorizer.authorizerId,
+			},
+		};
+
+		// Create the root resource and add methods and resources for the API
+
 		const spaceRss = api.root.addResource('spaces');
-		spaceRss.addMethod('GET', props.spacesLambdaIntegration);
-		spaceRss.addMethod('POST', props.spacesLambdaIntegration);
-		spaceRss.addMethod('PUT', props.spacesLambdaIntegration);
-		spaceRss.addMethod('DELETE', props.spacesLambdaIntegration);
+		spaceRss.addMethod('GET', props.spacesLambdaIntegration, optionsWithAuth);
+		spaceRss.addMethod('POST', props.spacesLambdaIntegration, optionsWithAuth);
+		spaceRss.addMethod('PUT', props.spacesLambdaIntegration, optionsWithAuth);
+		spaceRss.addMethod('DELETE', props.spacesLambdaIntegration, optionsWithAuth);
 	}
 }
